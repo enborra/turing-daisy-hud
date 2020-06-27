b55e3889b2bdce0a82f3280cdefc2a0ac58333d8
Andres <andres@enborra.com>
2020-06-27 00:07:26 -0700

    Update core_service.py

--- app/core/core_service.py
+++ app/core/core_service.py
@@ -11,27 +11,8 @@ from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
 from kivy.clock import Clock
 
+from display import Display
 
-class Display(App):
-    lbl = None
-    _i = 0
-
-    def Clock_Callback(self, dt):
-        self._i += 1
-        self.lbl.text = str(self._i)
-
-    def build(self):
-        self.box = BoxLayout(orientation='horizontal', spacing=20)
-        self.lbl = Label(
-            text='0°F',
-            font_size='150'
-        )
-
-        self.box.add_widget(self.lbl)
-
-        # Clock.schedule_interval(self.Clock_Callback, 1)
-
-        return self.box
 
 class CoreService(object):
     _comm_client = None
